87d9f21f770ce6b26dbde8056b60f3851f44bcf7
Loïc Morvan <loic.morvan.fr@gmail.com>
2020-01-14 23:57:05 +0100

    Added test for BusEx.Signal and moved the unit test projects to a solution folder.

---
+++ BreadBoard.UnitTests/BusExTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using Processor;
+using System;
+using Xunit;
+
+namespace BreadBoard.UnitTests
+{
+    public class BusExTests
+    {
+        [Fact]
+        public void SignalWorks()
+        {
+            var observerMock = new Mock<IObserver<bool>>();
+            var signalBus = new Bus<bool>();
+            signalBus.ValueChanged.Subscribe(observerMock.Object);
+            observerMock.Verify(o => o.OnNext(false));
+
+            signalBus.Signal();
+
+            observerMock.Verify(o => o.OnNext(true));
+            observerMock.Verify(o => o.OnNext(false));
+            observerMock.VerifyNoOtherCalls();
+        }
+    }
+}
